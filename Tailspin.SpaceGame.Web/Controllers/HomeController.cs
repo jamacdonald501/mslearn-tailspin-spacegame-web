91f75c01dc0d2ae3f7012d8416aa4f536e1b62e0
jamacdonald501 <jamacdonald@creativecomputingvab.com>
2020-07-29 13:33:59 -0400

    Add publish tasks

--- Tailspin.SpaceGame.Web/Controllers/HomeController.cs
+++ Tailspin.SpaceGame.Web/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using TailSpin.SpaceGame.Web.Models;
 
+
 namespace TailSpin.SpaceGame.Web.Controllers
 {
     public class HomeController : Controller
